000c82bc3c371822a629217c0402f9ee78c6c384
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-07 22:07:07 +0530

    Update main.v

--- src/main.v
+++ src/main.v
@@ -1,10 +1,10 @@
-module rgb_to_greyscale; 
+module top; 
 
 parameter m = 512;    //number of columns of image 
 parameter n = 512;    //number of rows of image
 
 reg  hexfile[m*n*3];    
-reg  grayfile[m*n];     
+reg  modfile[m*n];     
 reg  red_pixels[m*n];        
 reg  blue_pixels[m*n];         
 reg  green_pixels[m*n];    
