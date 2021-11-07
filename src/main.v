674b5be0c65717741aec38a152992b755940e6cb
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-07 21:29:53 +0530

    Create main.v

---
+++ src/main.v
@@ -0,0 +1,17 @@
+module rgb_to_greyscale; 
+
+parameter m = 512;    //number of columns of image 
+parameter n = 512;    //number of rows of image
+
+reg  hexfile[m*n*3];    
+reg  grayfile[m*n];     
+reg  red_pixels[m*n];        
+reg  blue_pixels[m*n];         
+reg  green_pixels[m*n];    
+integer file;//output file to write the hex code for image
+integer i, j, k=0;
+reg [9:0] val;
+
+initial begin 
+	$readmemh("image.hex", hexfile);        //reading hexadecimal file of image into memory hexfile
+end
