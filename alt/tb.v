e11973cadbd9c931432938b07c1f57745d2cf785
Abhijith T R <2020csb1062@iitrpr.ac.in>
2021-11-15 18:00:40 +0530

    Added brightness increase

--- alt/tb.v
+++
@@ -1,9 +0,0 @@
-module testbench();
-    reg brightness,inversion,threshold,updown; //All inputs (outputs entirely managed by the other file)
-
-    image_read im(brightness,inversion,threshold,updown) ;
-
-    initial begin
-    end
-
-endmodule
