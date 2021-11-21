a0a11b894a80526824e4abda77a43886c8d2ada3
Aditya Aggarwal <77119118+aggarwal-aditya@users.noreply.github.com>
2021-11-21 18:35:11 +0530

    Added Comments

--- main.v
+++ main.v
@@ -14,16 +14,20 @@ integer i, j, k=0;
 reg [9:0] val;
 
 initial begin 
-	$readmemh("image.hex", hexfile);        
+	$readmemh("image.hex", hexfile); //Reading hex file generated from MATLAB       
 end
 initial begin
 	for(i=0; i<m*n; i=i+1)begin
-			
-		red[i] = hexfile[k];           
+		
+		//Storing RGB values from the hexfile in three separate registers
+		red[i] = hexfile[k];          
 		green[i] = hexfile[k+1];
 		blue[i] = hexfile[k+2];
+		
+		//Assigning the average of RGB values of a pixel to grayfile i.e. the grayscale image
 		val = red[i]+green[i]+blue[i];    
-		grayfile[i] = (val)/8'h03;     
+		grayfile[i] = (val)/8'h03; 
+		
 		//grayfile[i] = 255-grayfile[i];  Uncomment to do inversion operation
 		k = k + 3;
 		
@@ -32,6 +36,7 @@ end
 initial begin
 	for(i=0;i<n;i=i+1)begin
 		for (j = 0; j < m/2; j++) begin
+			//Mirror Image of original image by swapping the appropriate hex values.
 			temp=grayfile[(m*i)+j];
 			grayfile[(m*i)+j]=grayfile[(m*i)+(m-1)-j];
 			grayfile[(m*i)+(m-1)-j]=temp;
